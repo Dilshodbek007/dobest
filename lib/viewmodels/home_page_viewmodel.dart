c460dd3dbc558bc84563961cf74b95e3a0e8731f
Dilshodbek007 <qurbonaliyevdil@gmail.com>
2022-05-21 16:21:43 +0500

    Initial commit

---
+++ lib/viewmodels/home_page_viewmodel.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dobest/service/api_response.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/currency_model.dart';
+
+class HomeViewModel extends ChangeNotifier {
+  ApiResponse _apiResponse = ApiResponse.initial('Empty');
+  ApiResponse get apiResponse {
+    return _apiResponse;
+  }
+
+  final List<CurrencyRate> _list = [];
+  List<CurrencyRate> get list {
+    return _list;
+  }
+
+  Future<ApiResponse> getData() async {
+    String url = 'https://nbu.uz/uz/exchange-rates/json/';
+    var myUrl = Uri.parse(url);
+    try {
+      var response = await http.get(myUrl);
+      List data = jsonDecode(response.body);
+      _list.clear();
+      for (var item in data) {
+        _list.add(CurrencyRate.fromJson(item));
+      }
+      _apiResponse = ApiResponse.success(_list);
+
+      return _apiResponse;
+    } on SocketException {
+      _apiResponse = ApiResponse.error('Socket Exception');
+
+      return _apiResponse;
+    } catch (error) {
+      _apiResponse = ApiResponse.error(error.toString());
+      return _apiResponse;
+    }
+  }
+}
